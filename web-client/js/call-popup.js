0ec8d69661d1d4846bec54cf22976cbf7e0c2569
raghavendra-degpeg <59719201+raghavendra-degpeg@users.noreply.github.com>
2024-09-17 22:52:28 +0530

    Update call-popup.js
    corrected path to html and img

--- web-client/js/call-popup.js
+++ web-client/js/call-popup.js
@@ -7,8 +7,8 @@ let isIntervalActive = true;
 let meetingDropped = false;
 
 let apiUrl = "https://dev.db.degpeg.com/";
-let imgAssetUrl = "https://cdn.jsdelivr.net/gh/degpeg-media/video-call-sdk-js@main/web-client/js/img";
-let htmlAssetUrl = "https://cdn.jsdelivr.net/gh/degpeg-media/video-call-sdk-js@main/web-client/js/html";
+let imgAssetUrl = "https://cdn.jsdelivr.net/gh/degpeg-media/video-call-sdk-js@main/web-client/img";
+let htmlAssetUrl = "https://cdn.jsdelivr.net/gh/degpeg-media/video-call-sdk-js@main/web-client/html";
 
 document.addEventListener("DOMContentLoaded", () => {
   socket = io("https://dev.backend.degpeg.com", {
